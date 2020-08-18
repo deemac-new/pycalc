a9099eb7e11dbd3440528ac116adde051a537329
deemac-new <dana.mcnamara@gmail.com>
2020-08-18 15:24:09 +1000

    d

--- calc.py
+++ calc.py
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
 # Perform simple arithmetic encoded in an input string:
 # '1 + 2' -> 3, or '1 - 2' -> -1.def compute(expression):
     values = expression.split(' ')
     num0 = int(values[0])
     operator = values[1]
     num1 = int(values[2])
+=======
+def compute(expression):
+num0, operator, num1 = expression.split(' ')
+	num0, num1 = int(num0), int(num1)
+>>>>>>> use-unpacking
     if operator == '+':
         return num0 + num1
 	elif operator == '-':
